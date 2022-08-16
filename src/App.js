9acca232e8b6f51c3f0d803b089f7d9bf879d4ce
Juhwa Hwang <zerothhan@gmail.com>
2022-08-16 15:55:57 +0900

    4. 현재위치 기반 날씨 가져오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
+// 1. 앱이 실행되자마자 현재위치 기반의 날씨가 보인다.
+// 2. 날씨 정보에는 도시, 섭씨, 화씨, 날씨 상태
+// 3. 5개의 버튼이 있다. (1개는 현재 위치, 4개는 다른 도시)
+// 4. 도시 버튼을 클릭할 때 마다 도시별 날씨가 나온다.
+// 5. 현재 위치 버튼을 누르면 다시 현재 위치 기반의 날씨가 나온다.
+// 6. 데이터를 들고 오는 동안 로딩 스피너가 돈다.
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let lon = position.coords.longitude;
+      getWeatherByCurrentLocation(lat, lon);
+    });
+  };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3c37ac29619b658f0d74f2acaac360b1`;
+
+      let response = await fetch(url);
+      let data = await response.json();
+
+      if (data.cod == 200) {
+        console.log("받은 데이터는", data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  return <div>hi</div>;
 }
 
 export default App;
